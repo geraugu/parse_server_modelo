e83b97f4cc49d5665a5876bd8fbb8f7fd94142ac
geraugu <geraldo.morais@gmail.com>
2016-03-15 14:39:16 +0000

    primeiro commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Exemplo para migrar o serviço parse para um servidor AWS.
---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict';
+//var _ = require("underscore");
+var express = require('express');
+var ParseServer = require('parse-server').ParseServer;
+var nconf = require('nconf');
+
+var app = express();
+
+nconf.argv().env().file({file: './config/default.json'});
+
+// Specify the connection string for your mongodb database
+// and the location to your Parse cloud code
+var api = new ParseServer({
+  databaseURI: nconf.get('databaseURI'),
+  cloud: nconf.get('cloud'),
+  appId: nconf.get('appId'),
+  masterKey: nconf.get('masterKey'),
+  fileKey: nconf.get('fileKey'),
+  serverURL: 'http://localhost:1337/appserver'
+
+});
+
+// Serve the Parse API on the /parse URL prefix
+app.use(nconf.get('parseAPIPrefix'), api);
+
+// Hello world
+app.get('/', function(req, res) {
+  res.status(200).send('Express is running here.');
+});
+
+var port = nconf.get('port');
+app.listen(port, function() {
+  console.log('parse-server-example running on port %s', port);
+});
